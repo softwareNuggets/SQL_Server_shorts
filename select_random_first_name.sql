04ba71cdce5250588b716b72904dff9bc78634a1
Software Nuggets <92217545+softwareNuggets@users.noreply.github.com>
2024-12-05 12:11:32 -0500

    Select a Random Value from a Table->Column

---
+++ select_random_first_name.sql
@@ -0,0 +1,16 @@
+--YouTube: @SoftwareNuggets
+
+use LearnSQL;
+
+
+declare @random_name		VARCHAR(20);
+
+SET @random_name = (
+    SELECT TOP 1 fname
+    FROM first_names
+    WHERE gender = 'F'
+    AND fname LIKE 'M%a%i%e%'
+    ORDER BY NEWID()  -- Randomly order the result
+);
+
+select @random_name
